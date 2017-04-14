25de8116d4984be04eb4a89ba08f211d92043c3b
Sanford Cluett <scluett@umass.edu>
2017-04-14 15:01:35 -0400

    express

--- server/src/server.js
+++ server/src/server.js
@@ -12,17 +12,19 @@ app.listen(3000, function () {
   console.log('Example app Listening on port 3000!');
 });
 
-app.post('/reverse', function (req, res) {
+app.use(bodyParser.text());
 
+
+// Handle POST /reverse [data]
+app.post('/reverse', function (req, res) {
+  // If the request came with text, then the text() middleware handled it
+  // and made `req.body` a string.
+  // Check that req.body is a string.
   if (typeof(req.body) === 'string') {
     var reversed = reverseString(req.body);
     res.send(reversed);
+  } else {
+    // POST did not contain a string. Send an error code back!
+    res.status(400).end();
   }
-
-else{
-  res.status(400).end();
-}
-
 });
-
-app.use(bodyParser.text());
